045c1dbbb9552aa0807c4dbb51745ba56c6fe277
johnwallie <36524295+johnwallie@users.noreply.github.com>
2018-02-17 10:03:35 -0500

    Version 1.1
      * Improved PEP 8 conformity.
      * Eliminated need for ImageMagick to support pipes. 
      * Made temp files get written to /tmp instead of working directory.

--- pyflatter.py
+++ pyflatter.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
-#PyFlatter by John W. Allie
-#www.johnwallie.com
+# PyFlatter by John W. Allie
+# www.johnwallie.com
 
-import Image, ImageDraw, ImageColor, random, sys, timeit, subprocess, os, argparse
+import Image, ImageDraw, ImageColor, random, timeit, subprocess, os, argparse
 
 starttime=timeit.default_timer()
 
-######Functions
+# Functions
+
 
 def validPercent(value):
-    #Evaluate an integer to see if it's a valid percentage between 1 and 99
+    """Evaluate an integer to see if it's a valid percentage between 1 and 99"""
     intval = int(value)
     if not 0 < intval < 100:
         raise argparse.ArgumentTypeError("Scale factor must be a percentage between 1 and 99")
@@ -17,39 +18,44 @@ def validPercent(value):
 
 
 def flood(posx,posy,color):
-    #flood an area starting at the specified pixel with the specified color
-    ImageDraw.floodfill(im, (posx,posy), color, border=None)
+    """flood an area starting at the specified pixel with the specified color"""
+    ImageDraw.floodfill(im, (posx, posy), color, border=None)
+
     
 def RandomColor():
-    #Generate a random color with HSV values. Brightness is locked between 23-75% to prevent very light and
-    #very dark colors from being produced. The returned value is encoded as RGB, not HSV.
+    """Generate a random color with HSV values. Brightness is locked between 23-75% to prevent very light and
+    very dark colors from being produced. The returned value is encoded as RGB, not HSV."""
     hue=random.randint(0,360)
     sat=random.randint(0,100)
     bright=random.randint(25,75)
     return ImageColor.getrgb("hsl("+str(hue)+","+str(sat)+"%,"+str(bright)+"%)")
 
 def getAllPixels():
-    #Evaluate every pixel in the image one by one
+    """Evaluate every pixel in the image one by one"""
+    print("Creating color islands...")
     dimensions=im.size
     
     for posy in range(dimensions[1]):
         for posx in range(dimensions[0]):
             CheckPixel(posx,posy)
+    print("Color islands completed at "+elapsed())
+
             
 def CheckPixel(posx,posy):
-    #Determine whether the pixels need to be flooded
+    """Determine whether the pixels need to be flooded"""
     pixelColor=(im.load()[posx,posy])
     if pixelColor==(255,255,255,255):
-        #If pixel is white, fill with a random color
+        # If pixel is white, fill with a random color
         flood(posx,posy,RandomColor())
     elif pixelColor==(0,0,0,255):
-        #If pixel is black, fill with transparency
+        # If pixel is black, fill with transparency
         flood(posx,posy,(255,255,255,0))
     else:
         pass
-    
+
+
 def elapsed():
-    #Return how much time has elapsed
+    """Return how much time has elapsed"""
     if timeit.default_timer()-starttime < 60:
         return str(int(timeit.default_timer()-starttime))+" seconds"
     else:
@@ -61,57 +67,53 @@ def elapsed():
 
 
 def closeGaps():
-    #Invoke ImageMagick to close the gaps in the image.
+    """Invoke ImageMagick to close the gaps in the image."""
     if GetImage():
         print("Closing gaps...")
-        subprocess.call(["convert "+args.infile+" -negate -morphology Close Square:3 magick_output.png"], shell=True)
-        subprocess.call(["mogrify -negate magick_output.png"], shell=True)
+        subprocess.call(["convert "+args.infile+" -negate -morphology Close Square:3 /tmp/magick_output.png"], shell=True)
+        subprocess.call(["mogrify -negate /tmp/magick_output.png"], shell=True)
+
     
 def voronoi():
-    #Create the file to be passed to the voronoi script
+    """Create the file to be passed to the voronoi script, then call the script"""
     if not args.novor:
         if not args.scaling:
-            im.save("voronoi_input.png", "PNG")
+            im.save("/tmp/voronoi_input.png", "PNG")
             print("Doing voronoi fill...")
             subprocess.call(["./voronoi.sh"], shell=True)
         else:
-            #If scaling option is in use, scale the file before passing it to voronoi script, then when the
-            #script has completed, scale it back up.
+            # If scaling option is in use, scale the file before passing it to voronoi script, then when the
+            # script has completed, scale it back up.
             scaleFactor=float(args.scaling)/100
             rsz = im.resize((int(im.size[0]*scaleFactor),int(im.size[1]*scaleFactor)))
-            rsz.save("voronoi_input.png", "PNG")
+            rsz.save("/tmp/voronoi_input.png", "PNG")
             print("Doing the voronoi thing, please wait")
             subprocess.call(["./voronoi.sh"], shell=True)
             FinalImage = Image.open("output.png")
             finalrsz = FinalImage.resize((im.size[0],im.size[1]))
             finalrsz.save("output.png", "PNG")            
     else:
-        #If voronoi is disabled, just save the final image.
+        # If voronoi is disabled, just save the final image.
         im.save("output.png", "PNG")
         
     CleanUp()
+
     
 def CleanUp():
-    #Get rid of temporary files if they exist, but don't crash if they don't
+    """Get rid of temporary files if they exist, but don't crash if they don't"""
     try:
-        os.remove("magick_output.png")
+        os.remove("/tmp/magick_output.png")
     except OSError:
         pass
     
     try:
-        os.remove("voronoi_input.png")
+        os.remove("/tmp/voronoi_input.png")
     except OSError:
         pass
-        
-    
-def FillRoutine():
-    #Look for white pixels, then look for black pixels
-    print("Creating color islands...")
-    getAllPixels()
-    print("Color islands completed at "+elapsed())
-    
+
+
 def GetImage():
-    #Check if an image exists, then open it
+    """Check if an image exists, then open it"""
     if os.path.exists(args.infile):
         global im
         im = Image.open(args.infile).convert('RGBA')
@@ -119,30 +121,32 @@ def GetImage():
     else:
         print("Uh-oh! "+args.infile+" does not exist!")
         quit()
+
         
-########Argparse options
+#   Argparse options
 parser = argparse.ArgumentParser(prog='PyFlatter', description="Create color flats for a black-and-white bitmap.")
 parser.add_argument('-g', '--no-gaps', help='Disable gap handling', action="store_true", dest="nogaps")
 parser.add_argument('-v', '--no-voronoi', help='Disable voronoi fill', action="store_true", dest="novor")
-parser.add_argument('-s', '--scale', help='Scale the image by a given percentage to speed up voronoi processing', type=validPercent, dest="scaling", metavar=('PERCENTAGE'))
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('-s', '--scale', help='Scale the image by a given percentage to speed up voronoi processing',
+                    type=validPercent, dest="scaling", metavar='PERCENTAGE')
+parser.add_argument('--version', action='version', version='%(prog)s 1.1')
 parser.add_argument('infile', help='Input file', type=str)
 
 args = parser.parse_args()
 
-######The actual script
+#   The actual script
 
 if args.nogaps:
-    #If we're not handling gaps
+    # If we're not handling gaps
     GetImage()
-    FillRoutine()
+    getAllPixels()
     voronoi()
     
 else:
-    #If we are handling gaps
+    # If we are handling gaps
     closeGaps()
-    im = Image.open("magick_output.png").convert('RGBA')
-    FillRoutine()
+    im = Image.open("/tmp/magick_output.png").convert('RGBA')
+    getAllPixels()
     voronoi()
 
 
--- voronoi.sh
+++ voronoi.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-convert voronoi_input.png \
+convert /tmp/voronoi_input.png \
 \( +clone -alpha extract -morphology edgein diamond:1 \) \
 -alpha off -compose copy_opacity -composite \
 -background black -alpha background -depth 8 txt:- |\
 grep -v "none" |\
-sed -n 's/^\(.*,.*\):.*[#][^ ]*  \(.*\)$/\1,\2/p' | \
-convert voronoi_input.png \
-\( +clone -alpha off -sparse-color voronoi '@-' \) \
+sed -n 's/^\(.*,.*\):.*[#][^ ]*  \(.*\)$/\1,\2/p' > /tmp/vortemp
+convert /tmp/voronoi_input.png \
+\( +clone -alpha off -sparse-color voronoi '@/tmp/vortemp' \) \
 +swap -compose over -composite output.png
+
+rm /tmp/vortemp
